9dad36a6f29d161e3b33390621ef7c82e3587556
david <davidawesomea@gmail.com>
2023-01-31 13:41:57 -0700

    Refactor rock_paper_scissors.rb

--- RB101/Lesson 2 - Small Programs/rock_paper_scissors.rb
+++ RB101/Lesson 2 - Small Programs/rock_paper_scissors.rb
@@ -14,11 +14,7 @@ def winner(first, second)
   end
   WIN_CONDITIONS.each do |conditions|
     if conditions.include?(first) && conditions.include?(second)
-      if conditions[0] == first
-        return first
-      else
-        return second
-      end
+      return conditions[0] == first ? first : second 
     end
   end
 end
@@ -26,11 +22,11 @@ end
 def display_results(player, computer)
   case winner(player, computer)
   when player
-    prompt("You won!")
+    print("You won!")
   when computer
-    prompt("You lost!")
+    print("You lost!")
   else
-    prompt("It's a tie!")
+    print("It's a tie!")
   end
 end
 
@@ -39,6 +35,11 @@ def get_choice
   loop do
     prompt("Choose one: #{CHOICES.join(', ')}")
     choice = gets.chomp.downcase
+
+    if choice == 's'
+      puts "Did you mean 'scissors' (sc) or 'spock' (sp)?"
+      next
+    end
 
     CHOICES.each do |option|
       if option =~ Regexp.new(choice)
@@ -50,8 +51,27 @@ def get_choice
   end
 end
 
+def get_answer?(text)
+  print text
+  puts " (y/n)"
+  choice = gets.chomp.downcase
+  if choice.start_with?('y')
+    true
+  else
+    false
+  end
+end
+
+puts "Welcome to RPSSL! (Rock Paper Scissors Spock Lizard)"
+if get_answer?("Would you like to read the rules?")
+  puts "Normal RPS rules apply, with the addition of Lizard and Spock:\n"\
+       " Scissors cuts Paper covers Rock crushes\n"\
+       " Lizard poisons Spock smashes Scissors\n"\
+       " decapitates Lizard eats Paper disproves\n"\
+       " Spock vaporizes Rock crushes Scissors."
+end
 loop do
-  prompt("Rock paper scissors! First to three points is the grand winner.")
+  puts "Begin! First to three points is the grand winner."
   player_score = 0
   computer_score = 0
 
@@ -60,14 +80,15 @@ loop do
     computer_choice = CHOICES.sample
     prompt("You chose #{choice}, computer chose #{computer_choice}.")
 
-    display_results(choice, computer_choice)
     case winner(choice, computer_choice)
     when choice
       player_score += 1
     when computer_choice
       computer_score += 1
     end
-    puts ""
+
+    display_results(choice, computer_choice)
+    puts " [PLAYER: #{player_score} | COMPUTER: #{computer_score}]"
 
     break if player_score == 3 || computer_score == 3
   end
@@ -78,7 +99,23 @@ loop do
     prompt("The computer was the first to achieve a score of 3! Nice Try!")
   end
 
-  prompt("Do you want to play again? (y/n)")
-  play_again = gets.chomp.downcase
-  break unless play_again.start_with?('y')
+  break unless get_answer?("Do you want to play again?")
 end
+puts "Goodbye, thanks for playing!"
+
+=begin
+Things to work on:
+- DONE: Require an explicit choice from user for "s", rather than defaulting to scissors.
+- DONE: Add a scoreboard so that user can tell the score
+- DONE: Give more info to the user in general, such as a welcome/goodbye message or displaying rules.
+
+- Work on the spacing to make the code more readable, remember to divide it by function sections.
+  - Concrete example: the case statement from 64-69 should have a blank line before and after,
+    so it doesn't look intertwined with display_results.
+- Extract more methods
+  - DONE: Example: extract a method for play again (break unless play_again?)
+
+- The winner method is quite complex, think about another way to figure out the winner and consider the tradeoffs.
+- Work through other sections of the code and consider other possible solutions/tradeoffs. 
+  - Example: get_choice method
+=end
