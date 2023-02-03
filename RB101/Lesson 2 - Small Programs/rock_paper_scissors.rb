b559d19f21dd93cf08f52e4bb0b411f4ea5e41d3
david <davidawesomea@gmail.com>
2023-02-03 13:04:25 -0700

    Refactor rock_paper_scissors.rb

--- RB101/Lesson 2 - Small Programs/rock_paper_scissors.rb
+++ RB101/Lesson 2 - Small Programs/rock_paper_scissors.rb
@@ -1,22 +1,19 @@
 CHOICES = %w(rock paper scissors lizard spock)
-WIN_CONDITIONS = [%w(paper rock), %w(rock scissors), %w(scissors paper),
-                  %w(spock rock), %w(paper spock), %w(spock scissors),
-                  %w(lizard paper), %w(rock lizard), %w(scissors lizard),
-                  %w(lizard spock)]
+WIN_CONDITIONS = {
+  "rock" => %w(scissors lizard),
+  "paper" => %w(rock spock),
+  "scissors" => %w(paper lizard),
+  "spock" => %w(scissors rock),
+  "lizard" => %w(spock paper)
+}
 
 def prompt(message)
   puts "=> #{message}"
 end
 
 def winner(first, second)
-  if first == second
-    return "tie"
-  end
-  WIN_CONDITIONS.each do |conditions|
-    if conditions.include?(first) && conditions.include?(second)
-      return conditions[0] == first ? first : second 
-    end
-  end
+  return "tie" if first == second
+  WIN_CONDITIONS[first].include?(second) ? first : second
 end
 
 def display_results(player, computer)
@@ -31,7 +28,6 @@ def display_results(player, computer)
 end
 
 def get_choice
-  choice = ''
   loop do
     prompt("Choose one: #{CHOICES.join(', ')}")
     choice = gets.chomp.downcase
@@ -39,12 +35,8 @@ def get_choice
     if choice == 's'
       puts "Did you mean 'scissors' (sc) or 'spock' (sp)?"
       next
-    end
-
-    CHOICES.each do |option|
-      if option =~ Regexp.new(choice)
-        return option
-      end
+    elsif choice != ''
+      CHOICES.each { |option| return option if option =~ Regexp.new(choice) }
     end
 
     prompt("Invalid choice!")
@@ -52,9 +44,9 @@ def get_choice
 end
 
 def get_answer?(text)
-  print text
-  puts " (y/n)"
+  puts "#{text} (y/n)"
   choice = gets.chomp.downcase
+
   if choice.start_with?('y')
     true
   else
@@ -70,10 +62,10 @@ if get_answer?("Would you like to read the rules?")
        " decapitates Lizard eats Paper disproves\n"\
        " Spock vaporizes Rock crushes Scissors."
 end
+
 loop do
   puts "Begin! First to three points is the grand winner."
-  player_score = 0
-  computer_score = 0
+  scores = { player: 0, computer: 0 }
 
   loop do
     choice = get_choice
@@ -82,18 +74,18 @@ loop do
 
     case winner(choice, computer_choice)
     when choice
-      player_score += 1
+      scores[:player] += 1
     when computer_choice
-      computer_score += 1
+      scores[:computer] += 1
     end
 
     display_results(choice, computer_choice)
-    puts " [PLAYER: #{player_score} | COMPUTER: #{computer_score}]"
+    puts " [PLAYER: #{scores[:player]} | COMPUTER: #{scores[:computer]}]"
 
-    break if player_score == 3 || computer_score == 3
+    break if scores[:player] == 3 || scores[:computer] == 3
   end
 
-  if player_score == 3
+  if scores[:player] == 3
     prompt("You were the first to achieve a score of 3! Good job!")
   else
     prompt("The computer was the first to achieve a score of 3! Nice Try!")
@@ -101,21 +93,5 @@ loop do
 
   break unless get_answer?("Do you want to play again?")
 end
+
 puts "Goodbye, thanks for playing!"
-
-=begin
-Things to work on:
-- DONE: Require an explicit choice from user for "s", rather than defaulting to scissors.
-- DONE: Add a scoreboard so that user can tell the score
-- DONE: Give more info to the user in general, such as a welcome/goodbye message or displaying rules.
-
-- Work on the spacing to make the code more readable, remember to divide it by function sections.
-  - Concrete example: the case statement from 64-69 should have a blank line before and after,
-    so it doesn't look intertwined with display_results.
-- Extract more methods
-  - DONE: Example: extract a method for play again (break unless play_again?)
-
-- The winner method is quite complex, think about another way to figure out the winner and consider the tradeoffs.
-- Work through other sections of the code and consider other possible solutions/tradeoffs. 
-  - Example: get_choice method
-=end
