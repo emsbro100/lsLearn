636e748afe4a1fc74f2d4d4a47bb1a2bb0f08329
david <davidawesomea@gmail.com>
2023-03-02 13:01:57 -0700

    Refactor tictactoe data structures

--- RB110/Lesson 3 - Slightly Larger Problems/tictactoe.rb
+++ RB110/Lesson 3 - Slightly Larger Problems/tictactoe.rb
@@ -56,6 +56,20 @@ def position_index(position)
   (position - 1).divmod(3)
 end
 
+def position_to_value(board, position)
+  index = position_index(position)
+  board[index[0]][index[1]]
+end
+
+def positions_to_values(board, array)
+  new_arr = []
+  array.each do |value|
+    index = position_index(value)
+    new_arr << board[index[0]][index[1]]
+  end
+  new_arr
+end
+
 def empty_positions(board)
   empty = []
   board.each_with_index do |row, y|
@@ -69,28 +83,80 @@ def update_board!(board, position, char)
   board[pos_index[0]][pos_index[1]] = char
 end
 
-def all_x_or_o(row)
+def all_x_or_o?(row)
   row.all?('X') || row.all?('O')
 end
 
-def check_board(board)
-  lines = [[board[0][0], board[1][1], board[2][2]], # Diagonal lines
-           [board[0][2], board[1][1], board[2][0]]]
-  lines.concat(board, board.transpose)
+def board_lines
+  lines = { diagonal: [[1, 5, 9], [3, 5, 7]],
+            horizontal: [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            vertical: [[1, 4, 7], [2, 5, 8], [3, 6, 9]] }
+end
 
-  lines.each { |line| return line[0] if all_x_or_o(line) } # Check for winners
-  return 'TIE' if !board.any? { |row| row.any?(' ') } # Check for tie
+def check_board(board)
+  lines = board_lines
+
+  lines.values.each do |line_group| # Check for winners
+    line_group.each do |line|
+      if all_x_or_o?(positions_to_values(board, line))
+        return position_to_value(board, line[0]) 
+      end
+    end
+  end
+  return 'TIE' unless board.any? { |row| row.any?(' ') } # Check for tie
   nil # No winner or tie
+end
+
+def joinor(arr, delimiter=', ', final_delimiter='or')
+  joined_str = arr[0].to_s
+  
+  (arr.size - 1).times do |num|
+    if arr.size == 2
+      joined_str << " #{final_delimiter} "
+    elsif num >= arr.size - 2
+      joined_str << "#{delimiter}#{final_delimiter} "
+    else
+      joined_str << delimiter
+    end
+    joined_str << arr[num + 1].to_s
+  end
+
+  joined_str
 end
 
 def player_turn(board)
   puts "Please enter the number of the position you want to put an X in:"
-  puts "(#{empty_positions(board).join(', ')})"
+  puts "(#{joinor(empty_positions(board))})"
   update_board!(board, get_position(empty_positions(board)), 'X')
 end
 
+def ai_simple(board)
+  empty_positions(board).sample
+end
+
+def ai_defensive(board)
+  lines = board_lines
+
+  lines.each do |line|
+    if line.any?(' ')
+      if line.count('X') == 5
+
+      end
+    end
+  end
+
+end
+
+def ai_offensive(board)
+
+end
+
+def ai_minimax(board)
+
+end
+
 def computer_turn(board)
-  position = empty_positions(board).sample
+  position = ai_simple(board)
   update_board!(board, position, 'O')
 end
 
@@ -133,13 +199,16 @@ def play_game
 
   display(board) if turn == "computer"
   print_result(check_board(board))
+
+  check_board(board)
 end
 
 RULES = [
   "Tic Tac Toe is a 2 player game played on a 3x3 board. Each player takes",
   "a turn and marks a square on the board. The first player to reach 3 squares",
   "in a row, including diagonals, wins. If all 9 squares are marked and no",
-  "player has 3 squares in a row, then the game is a tie."
+  "player has 3 squares in a row, then the game is a tie.",
+  "The grand winner is the first to 5 wins."
 ]
 
 puts "Welcome to Tic-Tac-Toe!"
@@ -154,7 +223,30 @@ if gets.chomp.downcase == 'y'
 end
 
 loop do
-  play_game
+  scores = { player: 0, computer: 0 }
+
+  loop do
+    case play_game
+    when 'X' then scores[:player] += 1
+    when 'O' then scores[:computer] += 1
+    end
+
+    puts "Player's score: #{scores[:player]}, Computer's score: "\
+         "#{scores[:computer]}"
+
+    case
+    when scores[:player] == 5
+      puts "You are the grand winner! Good job!"
+      break
+    when scores[:computer] == 5
+      puts "The computer is the grand winner! Better luck next time!"
+      break
+    end
+    
+    puts "Press enter to continue."
+    gets
+  end
+
   puts "Would you like to play again? (y/n)"
   break unless gets.chomp.downcase == 'y'
 end
